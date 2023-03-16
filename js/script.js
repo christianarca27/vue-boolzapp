8d7fb37d363601b2e3d9e8e5023b4c6522f90bcf
christianarca27 <christian.arca.mail@gmail.com>
2023-03-16 18:22:25 +0100

    feat: stile + generazione automatica lista contatti

--- js/script.js
+++ js/script.js
@@ -3,7 +3,32 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        
+        contactList: [
+            {
+                name: "Marco",
+                image: "https://picsum.photos/200",
+                lastMessage: "Ultimo messaggio inviato",
+                lastMessageTime: "12:00"
+            },
+            {
+                name: "Francesco",
+                image: "https://picsum.photos/200",
+                lastMessage: "Ultimo messaggio inviato",
+                lastMessageTime: "12:00"
+            },
+            {
+                name: "Luca",
+                image: "https://picsum.photos/200",
+                lastMessage: "Ultimo messaggio inviato",
+                lastMessageTime: "12:00"
+            },
+            {
+                name: "Christian",
+                image: "https://picsum.photos/200",
+                lastMessage: "Ultimo messaggio inviato",
+                lastMessageTime: "12:00"
+            }
+        ]
       }
     },
 
